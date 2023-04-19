001cc911dfd2ac2f60bb6e32df4166ecda61daa7
Alexis Giovoglanian <alexisgiovoglanian@southerncode.us>
2023-04-19 12:29:31 -0300

    Header links

--- packages/ui/Header.tsx
+++ packages/ui/Header.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 
+const VERCEL_URL : String = process.env.VERCEL_URL,
+
 export const Header = () => {
   return (
     <header className="header">
       <div className="container">
-        <Link href="/web" className="header-link">
+        <Link href={`${VERCEL_URL}/web`} className="header-link">
           Web
         </Link>
-        <Link href="/docs" className="header-link">
+        <Link href={`${VERCEL_URL}/docs`} className="header-link">
           Docs
         </Link>
       </div>
