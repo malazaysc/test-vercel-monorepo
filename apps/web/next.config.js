912983e0de1b557900f797a5da7044611a762bd6
Alexis Giovoglanian <alexisgiovoglanian@southerncode.us>
2023-04-19 11:31:02 -0300

    Adding rewrites to web

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,22 @@
+const DOCS_BASE_URL = 'https://test-vercel-monorepo-docs.vercel.app'
+
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  async rewrites() {
+    return [
+      {
+        source: "/docs",
+        destination: `${DOCS_BASE_URL}/docs`,
+      },
+      {
+        source: "/docs/:path*",
+        destination: `${DOCS_BASE_URL}/docs/:path*`,
+      },
+      {
+        source: "/:path*",
+        destination: `https://test-vercel-monorepo-web.vercel.app/:path*`,
+      },
+    ];
+  },
 };
