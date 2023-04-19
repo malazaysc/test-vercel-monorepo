d02f08c9339abf8a12502999f59d35c740ae35ea
Alexis Giovoglanian <alexisgiovoglanian@southerncode.us>
2023-04-19 13:16:06 -0300

    Header updates

--- packages/ui/Header.tsx
+++ packages/ui/Header.tsx
@@ -6,11 +6,14 @@ export const Header = () => {
   return (
     <header className="header">
       <div className="container">
-        <Link href={`${VERCEL_URL}/web`} className="header-link">
+        <Link href={`${VERCEL_URL}`} className="header-link">
           Web
         </Link>
         <Link href={`${VERCEL_URL}/docs`} className="header-link">
           Docs
+        </Link>
+        <Link href={`${VERCEL_URL}/blog`} className="header-link">
+          {VERCEL_URL}
         </Link>
       </div>
     </header>
