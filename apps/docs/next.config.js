fc912d15d0a6e37cba3f4b37fca666b595d8a2ae
Alexis Giovoglanian <alexisgiovoglanian@southerncode.us>
2023-04-20 12:05:45 -0300

    preview magic

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -8,7 +8,7 @@ module.exports = {
     isPreview: process.env.VERCEL_ENV === 'preview',
     gitBranch: process.env.VERCEL_GIT_COMMIT_REF,
     VERCEL_URL: process.env.VERCEL_ENV === 'preview'
-    ? `https://test-vercel-monorepo-${gitBranch}-malazaysc.vercel.app`
+    ? `https://test-vercel-monorepo-${process.env.VERCEL_GIT_COMMIT_REF}-malazaysc.vercel.app`
     : 'https://test-vercel-monorepo-web.vercel.app',
     //https://test-vercel-monorepo-web-git-feature-test-preview-malazaysc.vercel.app/
     // VERCEL_URL: process.env.VERCEL_URL && `https://${process.env.VERCEL_URL}`,
