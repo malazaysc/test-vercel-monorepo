6473186121a31dcda8bf5a5f242e94b9d201a4a8
Alexis Giovoglanian <alexisgiovoglanian@southerncode.us>
2023-04-20 15:30:04 -0300

    Preview should work now

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,14 @@
-const DOCS_BASE_URL = 'https://test-vercel-monorepo-docs.vercel.app'
+const GIT_BRANCH = process.env.VERCEL_GIT_COMMIT_REF.replace('/', '-');
+const IS_PREVIEW = process.env.VERCEL_ENV === 'preview';
+
+const DOCS_BASE_URL = IS_PREVIEW ? `https://test-vercel-monorepo-docs-git-${GIT_BRANCH}-malazaysc.vercel.app` : 'https://test-vercel-monorepo-docs.vercel.app';
+const WEB_BASE_URL = IS_PREVIEW ? `https://test-vercel-monorepo-web-git-${GIT_BRANCH}-malazaysc.vercel.app` : 'https://test-vercel-monorepo-web.vercel.app';
+
+// const DOCS_BASE_URL = 'https://test-vercel-monorepo-docs.vercel.app'
+
+// const productionURL = 'https://test-vercel-monorepo-web.vercel.app';
+
+// const previewURL = `https://test-vercel-monorepo-web-git-${gitBranch}-malazaysc.vercel.app`;
 
 module.exports = {
   reactStrictMode: true,
@@ -15,7 +25,7 @@ module.exports = {
       },
       {
         source: "/:path*",
-        destination: `https://test-vercel-monorepo-web.vercel.app/:path*`,
+        destination: `${WEB_BASE_URL}/:path*`,
       },
     ];
   },
